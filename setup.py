b97f131c5fff0c9e39dbc57786c9de1563a94752
Chris Conley <chris.m.conley@gmail.com>
2012-12-07 22:37:35 -0500

    getting a little more functional

--- setup.py
+++ setup.py
@@ -21,44 +21,67 @@
 #g.process_letter_guess('o')
 #print g.to_s()
 
-import string
-class Game:
-    def __init__(self, word, rules):
-        """docstring for __init__"""
-        self.word = word
-        self.rules = rules
-        self.current_guess = ['-' for letter in word]
-        self.missed_letters = []
-        self.guessed_words = []
+#import string
+#class Game:
+    #def __init__(self, word, rules):
+        #"""docstring for __init__"""
+        #self.word = word
+        #self.rules = rules
+        #self.current_guess = ['-' for letter in word]
+        #self.missed_letters = []
+        #self.guessed_words = []
 
-    def to_s(self):
-        """docstring for to_s"""
-        template = string.Template("${current_guess} | ${missed_letters} | ${guessed_words}")
-        return template.substitute(self.__dict__)
+    #def to_s(self):
+        #"""docstring for to_s"""
+        #template = string.Template("${current_guess} | ${missed_letters} | ${guessed_words}")
+        #return template.substitute(self.__dict__)
 
 
-def new_game(word, rules):
-    """docstring for new_game"""
-    return Game(word, rules)
+#def new_game(word, rules):
+    #"""docstring for new_game"""
+    #return Game(word, rules)
 
-def calculate_score(game):
-    """docstring for calcluate_score"""
-    return (len(game.missed_letters) * game.rules['letter_miss']) + (len(game.guessed_words) * game.rules['word_miss'])
+#def calculate_score(game):
+    #"""docstring for calcluate_score"""
+    #return (len(game.missed_letters) * game.rules['letter_miss']) + (len(game.guessed_words) * game.rules['word_miss'])
 
-def process_letter_guess(game, guess):
-    """docstring for process_letter_guess"""
-    for index, letter in enumerate(game.word):
-        if guess == letter: game.current_guess[index] = guess
+#def process_letter_guess(game, guess):
+    #"""docstring for process_letter_guess"""
+    #for index, letter in enumerate(game.word):
+        #if guess == letter: game.current_guess[index] = guess
 
-    if not guess in game.word:
-        game.missed_letters.append(guess)
+    #if not guess in game.word:
+        #game.missed_letters.append(guess)
 
-rules = {'letter_miss': 1, 'letter_hit': 0, 'word_miss': 1, 'max_word_guesses': 5, 'max_word_guess_score': 25}
-g = new_game('hello', rules)
+#rules = {'letter_miss': 1, 'letter_hit': 0, 'word_miss': 1, 'max_word_guesses': 5, 'max_word_guess_score': 25}
+#g = new_game('hello', rules)
 
-process_letter_guess(g, 'h')
-process_letter_guess(g, 'x')
-process_letter_guess(g, 'o')
-process_letter_guess(g, 'n')
-print g.to_s()
-print calculate_score(g)
+#process_letter_guess(g, 'h')
+#process_letter_guess(g, 'x')
+#process_letter_guess(g, 'o')
+#process_letter_guess(g, 'n')
+#print g.to_s()
+#print calculate_score(g)
+
+def score(word, guesses):
+    """docstring for score"""
+    pass
+
+def guess(current, word, guesses):
+    """docstring for guess"""
+    words = [guess for guess in guesses if len(guess) == word]
+    letters = [guess for guess in guesses if len(guess) != word]
+    new = list(current)
+    if word in guesses:
+        return [word, guesses]
+    else:
+        for guess in letters:
+            for index, letter in enumerate(word):
+                if guess == letter: new[index] = guess
+        return (''.join(new), guesses)
+
+print guess("---", "cat", ["a"])
+print guess("-a-", "cat", ["a", 'x'])
+print guess("-a-", "cat", ["a", 'x', 't'])
+print guess("-at", "cat", ["a", 'x', 't', 'bat'])
+print guess("-at", "cat", ["a", 'x', 't', 'bat', 'cat'])
