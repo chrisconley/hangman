ea1e50afaa36b718b7d7ff848bf8c70e95464cc1
Chris Conley <chris.m.conley@gmail.com>
2014-01-12 14:29:05 -0500

    added first learner

---
+++ hangman/strat.py
@@ -0,0 +1,57 @@
+from collections import Counter
+import itertools
+import re
+import unittest
+
+from hangman.game import MysteryString
+
+def learn_word(word, counts={}):
+    for length in range(0, len(word)+1):
+        combinations = itertools.combinations(word, length)
+        for subset in combinations:
+            ms = MysteryString(word, set(subset))
+
+
+            # if our matched letters don't match our current subset,
+            # then move to the next subset.
+            #
+            # Ex:
+            #
+            # For the word 'sites', we will see ('s',) and ('s','s')
+            # as subsets, but we only want to continue on with ('s', 's')
+            # because our only valid MysteryString is 's---s'.
+            if tuple(re.sub('-', '', ms)) != subset:
+                continue
+
+            counter = counts.setdefault(ms, Counter())
+            for letter in set(word):
+                if letter not in subset and len(subset):
+                    if ms == 's---s':
+                        print subset, letter
+                    counter[letter] += 1
+    return counts
+
+#words = ['synth', 'pyscho', 'sites', 'siete']
+
+#for word in words:
+    #learn_word(word)
+
+#for k, v in counts.items():
+    #print k, v
+
+class Tests(unittest.TestCase):
+
+    def test_duplicate_letters(self):
+        counts = learn_word('sites')
+        self.assertIsNone(counts.get('s----'))
+        self.assertEqual(counts.get('s---s'), Counter({'i': 1, 'e': 1, 't': 1}))
+
+    def test_duplicate_letters(self):
+        counts = learn_word('synth')
+        counts = learn_word('siete')
+        expected_counter = Counter({'t': 2, 'e': 1, 'i': 1, 'h': 1, 'n': 1, 'y': 1}) 
+        self.assertEqual(counts.get('s----'), expected_counter)
+
+if __name__ == '__main__':
+    unittest.main()
+
