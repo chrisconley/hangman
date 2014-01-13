e462df5832e685d1dcbb75e03c0ab2bba4649fcf
Chris Conley <chris.m.conley@gmail.com>
2014-01-12 21:37:55 -0500

    switch to ints and array

--- hangman/strat.py
+++ hangman/strat.py
@@ -2,8 +2,41 @@ from collections import Counter
 import itertools
 import re
 import unittest
+import hashlib
 
 from hangman.game import MysteryString
+
+LETTER_MAP = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 8,
+    'j': 9,
+    'k': 10,
+    'l': 11,
+    'm': 12,
+    'n': 13,
+    'o': 14,
+    'p': 15,
+    'q': 16,
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'v': 21,
+    'w': 22,
+    'x': 23,
+    'y': 24,
+    'z': 25
+}
+
+def get_int_hash(key):
+    return int(hashlib.md5(key).hexdigest(), 16)
 
 def learn_word(word, counts={}):
     for length in range(0, len(word)+1):
@@ -14,40 +47,40 @@ def learn_word(word, counts={}):
                 continue
             ms = MysteryString(word, set(subset))
 
-            counter = counts.setdefault(ms, Counter())
+            ms_hash = get_int_hash(ms)
+            counter = counts.setdefault(ms_hash, [0]*26)
             remaining_letters = word_set.difference(subset)
             for letter in remaining_letters:
-                counter[letter] += 1
+                counter[LETTER_MAP[letter]] += 1
     return counts
-
-#words = ['synth', 'pyscho', 'sites', 'siete']
-
-#for word in words:
-    #learn_word(word)
-
-#for k, v in counts.items():
-    #print k, v
 
 class Tests(unittest.TestCase):
 
     def test_duplicate_letters(self):
         counts = learn_word('sites')
-        self.assertIsNone(counts.get('s----'))
-        self.assertEqual(counts.get('s---s'), Counter({'i': 1, 'e': 1, 't': 1}))
+
+        key = get_int_hash('s----')
+        self.assertIsNone(counts.get(key))
+
+        key = get_int_hash('s---s')
+        expected_counter = [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(counts.get(key), expected_counter)
 
     def test_overlapping_words(self):
         counts = learn_word('synth')
         counts = learn_word('siete')
-        expected_counter = Counter({'t': 2, 'e': 1, 'i': 1, 'h': 1, 'n': 1, 'y': 1}) 
-        self.assertEqual(counts.get('s----'), expected_counter)
+
+        key = get_int_hash('s----')
+        expected_counter = [0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0]
+        self.assertEqual(counts.get(key), expected_counter)
 
 if __name__ == '__main__':
-    with open('words-short.txt', 'r') as f:
-        counts = {}
-        for word in f:
-            #print word.strip()
-            counts = learn_word(word, counts)
-        print len(counts)
+    import fileinput
+    counts = {}
+    for word in fileinput.input():
+        learn_word(word.strip(), counts)
+    print len(counts)
+
     unittest.main()
 
 
