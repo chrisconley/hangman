00e74e9ba92857b12ac29bdcae818f70d33a3e35
Chris Conley <chris.m.conley@gmail.com>
2014-02-07 23:14:02 -0500

    added an ugly most_entropy method

--- lookahead/play.py
+++ lookahead/play.py
@@ -1,7 +1,33 @@
 from collections import Counter
+import math
 from hangman import game
 
 COUNTERS = {}
+
+def most_entropy(counters):
+    word_count = counters['*']
+    entropies = Counter()
+    for letter, counter in counters.items():
+        if letter == '*':
+            continue
+        total = counter['*']
+        miss_plausibility = float(word_count - total)/float(word_count)
+        plausibilities = []
+        if miss_plausibility != 0:
+            plausibilities.append(miss_plausibility * math.log(miss_plausibility))
+            for subset, count in counter.items():
+                if subset == '*':
+                    continue
+                p = float(count) / float(word_count)
+                plausibilities.append(p * math.log(p))
+
+            entropy = -1.0 * sum(plausibilities)
+            entropies[letter] = entropy
+        else: # we know this letter is a match because there are no remaining misses possible
+            # This is sorta cheating because entropy should actually be 0
+            entropies[letter] = 1000000000
+    for letter, count in entropies.most_common():
+        yield letter, count
 
 def duplicates_strategy(previous_result):
     word_length = len(previous_result)
@@ -9,7 +35,7 @@ def duplicates_strategy(previous_result):
     counter = COUNTERS[word_length][key]
 
     # TODO: choose max entropy instead of most_common
-    for letter, count in counter.most_common():
+    for letter, count in most_entropy(counter):
         if letter not in previous_result.guesses and letter != '*':
             return previous_result.guesses | set(letter)
 
