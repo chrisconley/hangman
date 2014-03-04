9f050d47b21dd9567da72b718f40c9bce8cd2af7
Chris Conley <chris.m.conley@gmail.com>
2014-03-04 08:36:15 -0500

    remove setup.sh and reset aws credentials :P

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# AWS IAM Username: chrisconley-io
-# For boto
-export AWS_ACCESS_KEY_ID=AKIAJKL4F5AYCQ5SCO2Q
-export AWS_SECRET_ACCESS_KEY=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
-# For aws command line tools
-export AWS_ACCESS_KEY=AKIAJKL4F5AYCQ5SCO2Q
-export AWS_SECRET_KEY=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
-
-ssh-add ~/.ssh/chrisconley-io.pem
