1eea1f9d92d3c0e19c411b75a2aac9e2c1563e8e
Chris Conley <pair+chrisconley@realscout.com>
2016-10-20 13:33:47 -0700

    add hangman state tests

---
+++ hangman_opponent_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from hangman_opponent import HangmanGameState
+
+
+class HangmanGameStateTests(unittest.TestCase):
+    def test_string(self):
+        state = HangmanGameState('banana', set())
+        self.assertEqual(state, '------')
+
+        state = HangmanGameState('banana', set('c'))
+        self.assertEqual(state, '------')
+
+        state = HangmanGameState('banana', set('cbn'))
+        self.assertEqual(state, 'b-n-n-')
+
+    def test_missed_letters(self):
+        state = HangmanGameState('banana', set())
+        self.assertEqual(state.missed_letters, set())
+
+        state = HangmanGameState('banana', set('bn'))
+        self.assertEqual(state.missed_letters, set())
+
+        state = HangmanGameState('banana', set('bnce'))
+        self.assertEqual(state.missed_letters, set('ce'))
