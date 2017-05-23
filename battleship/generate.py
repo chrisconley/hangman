80d42b50fab79e76200a086e113b00909f1abd35
Chris Conley <pair+chrisconley@realscout.com>
2017-05-22 20:38:00 -0700

    Board is finished, working on battleship generator still

--- battleship/generate.py
+++ battleship/generate.py
@@ -1,7 +1,13 @@
+import numpy as np
+
 
 class Board(object):
-    def __init__(self, size=(3,3)):
+    def __init__(self, size):
+        self.width = size[0]
+        self.height = size[1]
         self._board = None
+        self._layout = np.zeros(self.width * self.height, dtype='int8')
+        self._ships = []
         pass
 
     @property
@@ -12,8 +18,42 @@ class Board(object):
         """
         Return None if ship cannot be placed
         """
-        self._board
-        pass
+        ship = None
+        if orientation == 'H':
+            zeros = np.zeros(self.width * self.height, dtype='int8')
+            if (position[0] + length) > self.width:
+                return None
+            for i in range(length):
+                zeros[position[1] * self.width + position[0]+i] = 1
+            if np.all(np.bitwise_and(self._layout, zeros) == 0):
+                self._layout = np.bitwise_or(self._layout, zeros)
+                ship = Ship(position, length, orientation)
+        elif orientation == 'V':
+            zeros = np.zeros(self.width * self.height, dtype='int8')
+            if (position[1] + length) > self.height:
+                return None
+            for i in range(length):
+                zeros[(position[1] + i) * self.width + position[0]] = 1
+            if np.all(np.bitwise_and(self._layout, zeros) == 0):
+                self._layout = np.bitwise_or(self._layout, zeros)
+                ship = Ship(position, length, orientation)
+        if ship:
+            self._ships.append(ship)
+            return ship
+
+    def get_next_position(self, start_position):
+        start_column, start_row = start_position
+        if start_column >= self.width or start_row >= self.height:
+            raise
+        if start_column == self.width - 1 and start_row == self.height - 1:
+            return None
+        elif start_column == self.width - 1:
+            return [0, start_row + 1]
+        return [start_position[0]+1, start_position[1]]
+
+    @property
+    def layout(self):
+        return np.reshape(self._layout, (self.height, self.width)).tolist()
 
 
 class Ship(object):
@@ -37,7 +77,7 @@ def run(ships, board):
       What do we do when b) puts us off the last position of the board
       How do we handle ships that can't be place anywhere
     """
-    current_ship = Ship((0,0), ships[0], 'H')
+    current_ship = Ship([0, 0], ships[0], 'H')
     return _run(
         desired_num_ships=len(ships),
         ships_to_be_placed=ships,
@@ -51,29 +91,29 @@ def _run(desired_num_ships, ships_to_be_placed, ships_already_placed, board, cur
     placed_ship = board.place_ship(current_ship.position, current_ship.length, current_ship.orientation)
 
     if placed_ship is None:  # we couldn't place ship
-        return None
+        return []
 
     # If we've placed all ships, return the board
     if board.num_ships == desired_num_ships:
-        return board
+        return [board]
 
     # If we have been able to place all ships, then exit out
-    if board.num_ships != desired_num_ships and len(ships_to_be_placed) == 0:
-        return None
+    # if board.num_ships != desired_num_ships and len(ships_to_be_placed) == 0:
+    #     return []
 
     # If we still need to place more ships
     if len(ships_to_be_placed) >= 1:
         next_position = board.get_next_position(current_ship.position)
-        next_ship = Ship(next_position, ships_to_be_placed[1:], 'H')
-        return _run(desired_num_ships, ships_to_be_placed[1:], ships_already_placed, board, next_ship)
+        next_ship = Ship(next_position, ships_to_be_placed[0], 'H')
+        return [board] + _run(desired_num_ships, ships_to_be_placed[1:], ships_already_placed, board, next_ship)
 
     next_position = board.get_next_position(current_ship.position)
     if current_ship.is_horizontal:
-        return _run(desired_num_ships, ships_to_be_placed, ships_already_placed, board, current_ship)
+        return [board] + _run(desired_num_ships, ships_to_be_placed, ships_already_placed, board, current_ship)
     elif next_position is None: # we've reached end of board and haven't been able to place all ships
-        return None
+        return []
     else:
         next_ship = Ship(next_position, current_ship.length, 'H')
-        return _run(desired_num_ships, ships_to_be_placed, ships_already_placed, board, current_ship)
+        return [board] + _run(desired_num_ships, ships_to_be_placed, ships_already_placed, board, current_ship)
     # if current_ship:
     #     _run(ships_to_be_placed[1:], ships_already_placed.append(current_ship), board)
--- battleship/generate_test.py
+++ battleship/generate_test.py
@@ -3,8 +3,172 @@ import unittest
 from battleship import generate
 
 
+class BoardTests(unittest.TestCase):
+    def test_place_ship_horizontal(self):
+        board = generate.Board(size=[3, 3])
+        ship = board.place_ship((0, 0), 2, 'H')
+        self.assertEqual(ship.position, (0, 0))
+        self.assertEqual(ship.length, 2)
+        self.assertEqual(ship.orientation, 'H')
+
+    def test_place_ship_horizontal_overlapping(self):
+        board = generate.Board(size=[4, 3])
+        board.place_ship((0, 0), 2, 'H')
+        ok_ship = board.place_ship((2, 0), 2, 'H')
+        self.assertEqual(board.layout, [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ])
+
+        partially_overlapping_ship = board.place_ship((1, 0), 2, 'H')
+        completely_overlapping_ship = board.place_ship((0, 0), 2, 'H')
+        self.assertIsNotNone(ok_ship)
+        self.assertIsNone(partially_overlapping_ship)
+        self.assertIsNone(completely_overlapping_ship)
+
+    def test_place_ship_horizontal_outside_right(self):
+        board = generate.Board(size=[3, 3])
+        ok_ship = board.place_ship((1, 0), 2, 'H')
+        outside_ship = board.place_ship((2, 0), 2, 'H')
+        self.assertIsNotNone(ok_ship)
+        self.assertIsNone(outside_ship)
+        
+    def test_place_ship_vertical(self):
+        board = generate.Board(size=[3, 3])
+        ship = board.place_ship((0, 0), 2, 'V')
+        self.assertEqual(ship.position, (0, 0))
+        self.assertEqual(ship.length, 2)
+        self.assertEqual(ship.orientation, 'V')
+
+    def test_place_ship_vertical_overlapping(self):
+        board = generate.Board(size=[3, 4])
+        board.place_ship((0, 0), 2, 'V')
+        ok_ship = board.place_ship((0, 2), 2, 'V')
+
+        self.assertEqual(board.layout, [
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+        ])
+        partially_overlapping_ship = board.place_ship((0, 1), 2, 'V')
+        completely_overlapping_ship = board.place_ship((0, 0), 2, 'V')
+        self.assertIsNotNone(ok_ship)
+        self.assertIsNone(partially_overlapping_ship)
+        self.assertIsNone(completely_overlapping_ship)
+
+    def test_place_ship_vertical_outside_bottom(self):
+        board = generate.Board(size=[3, 3])
+        ok_ship = board.place_ship((0, 1), 2, 'V')
+        outside_ship = board.place_ship((0, 2), 2, 'V')
+        self.assertIsNotNone(ok_ship)
+        self.assertIsNone(outside_ship)
+
+    def test_num_ships_initial(self):
+        board = generate.Board(size=[3, 3])
+        self.assertEqual(board.num_ships, 0)
+
+    def test_num_ships_after_placement(self):
+        board = generate.Board(size=[3, 3])
+        board.place_ship((0, 0), 2, 'H')
+        self.assertEqual(board.num_ships, 1)
+
+    def test_layout_initial(self):
+        board = generate.Board(size=[3, 3])
+        self.assertEqual(board.layout, [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ])
+
+    def test_layout_after_horizontal_placement(self):
+        board = generate.Board(size=[3, 3])
+        board.place_ship((0, 0), 2, 'H')
+        self.assertEqual(board.layout, [
+            [1, 1, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ])
+
+        board = generate.Board(size=[3, 3])
+        board.place_ship((1, 1), 2, 'H')
+        self.assertEqual(board.layout, [
+            [0, 0, 0],
+            [0, 1, 1],
+            [0, 0, 0],
+        ])
+
+        board = generate.Board(size=[5, 4])
+        board.place_ship((3, 2), 2, 'H')
+        self.assertEqual(board.layout, [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0],
+        ])
+
+    def test_layout_after_vertical_placement(self):
+        board = generate.Board(size=[3, 3])
+        board.place_ship((0, 0), 2, 'V')
+        self.assertEqual(board.layout, [
+            [1, 0, 0],
+            [1, 0, 0],
+            [0, 0, 0],
+        ])
+
+        board = generate.Board(size=[3, 3])
+        board.place_ship((1, 1), 2, 'V')
+        self.assertEqual(board.layout, [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 1, 0],
+        ])
+
+        board = generate.Board(size=[4, 5])
+        board.place_ship((2, 3), 2, 'V')
+        self.assertEqual(board.layout, [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+        ])
+
+    def test_get_next_position_wide_board(self):
+        board = generate.Board(size=[3, 2])
+        result = board.get_next_position([0, 0])
+        self.assertEqual(result, [1, 0])
+
+        result = board.get_next_position([2, 0])
+        self.assertEqual(result, [0, 1])
+
+        result = board.get_next_position([2, 1])
+        self.assertEqual(result, None)
+
+    def test_get_next_position_tall_board(self):
+        board = generate.Board(size=[2, 3])
+        result = board.get_next_position([0, 0])
+        self.assertEqual(result, [1, 0])
+
+        result = board.get_next_position([1, 0])
+        self.assertEqual(result, [0, 1])
+
+        result = board.get_next_position([1, 2])
+        self.assertEqual(result, None)
+
+    def test_get_next_position_invalid_inputs(self):
+        board = generate.Board(size=[2, 3])
+        with self.assertRaises(RuntimeError):
+            board.get_next_position([2, 0])
+
+        with self.assertRaises(RuntimeError):
+            board.get_next_position([0, 3])
+
+
 class GenerateTests(unittest.TestCase):
     def test_run(self):
-        board = generate.Board(size=(3, 3))
-        result = generate.run(ships=(2, 2), board=board)
-        self.assertEqual(result, [])
+        board = generate.Board(size=[3, 3])
+        results = generate.run(ships=[2, 2], board=board)
+        layouts = [b.layout for b in results]
+        self.assertEqual(layouts, [1])
