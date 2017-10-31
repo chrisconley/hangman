98fdf12e9f341fd45385272e039a507e009a47dd
Chris Conley <pair+chrisconley@realscout.com>
2017-10-31 16:34:14 -0700

    cleaned up analyze a bit

--- games/mastermind/analyze_strategy.py
+++ games/mastermind/analyze_strategy.py
@@ -5,33 +5,40 @@ def play(code_word, strategy, index, response_sentinel):
     guesses = []
     strategy_place = strategy['']
     while True:
+        # For compact representations where they don't give us the final correct code word
+        # add a response for correct counting purposes
         if type(strategy_place) == int and strategy_place == 1:
-            # For compact representations where they don't give us the final correct code word
-            # add a response for correct counting purposes
             guesses.append('gotit')
             break
 
-        if strategy_place[1] < 'A':
+        # If this "guess" is a reference to a sub-tree, then update where
+        # we are in the strategy tree.
+        guess = strategy_place[1]
+        if guess >= 'A':
+            strategy_place = strategy[guess]
             guess = strategy_place[1]
-        else:
-            strategy_place = strategy[strategy_place[1]]
-            guess = strategy_place[1]
+
         response = opponent.get_response(code_word, guess)
 
-        if type(strategy_place) == list and len(strategy_place) == 2:
-            # For compact representations where they don't give us the final correct code word
-            # add a response for this turn and the final turn for correct counting purposes
-            # Ex: [4, '3456']
+        # For compact representations where they don't give us the final correct code word
+        # add a response for this turn. If the response doesn't match the sentinel, then we'll need
+        # an additional guess appended to our list of guesses.
+        # Ex: [4, '3456']
+        if len(strategy_place) == 2:
             guesses.append('gotit')
             if response != response_sentinel:
                 guesses.append('gotit')
             break
 
-        strategy_place = strategy_place[2][index[response]]
+        # Append the actual guess and exit if we've correctly guessed the codeword.
         guesses.append(guess)
         if response == response_sentinel:
             assert guess == code_word
             break
+
+        # Update our place in the strategy tree for the next while loop iteration
+        strategy_place = strategy_place[2][index[response]]
+
     return guesses
 
 
